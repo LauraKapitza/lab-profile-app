c311d6e6e207440339edbfe34be434efd247618f
Laura Kapitza <laura.kapitza@gmail.com>
2021-07-10 15:01:18 +0200

    iteration 2 wip

---
+++ client/src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Home extends React.Component {
+    render() {
+        return(
+            <div className='Homepage'>
+                <h2>IronProfile</h2>
+                <p>Today we will create an app with authoritation, adding some cool styles!</p>
+                <button>Signup</button>
+                <button>Login</button>
+            </div>
+        )
+    }
+}
+
+export default Home;
---
+++ client/src/components/auth/Signup.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {signup} from './auth-service';
+
+class Signup extends React.Component {
+    state = {
+        username: '',
+        passwors: ''
+    }
+
+    //handleChange() and hazndleSubmit() will be added here
+
+    render() {
+        return (
+            <div>coucou</div>
+        )
+    }
+}
+
+export default Signup;
