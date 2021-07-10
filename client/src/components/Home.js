6a6eb6ce2b530cd95b8c1f24eebad9f7974eedf0
Laura Kapitza <laura.kapitza@gmail.com>
2021-07-10 21:43:30 +0200

    iteration 2 done

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React from 'react';
+import '../components/components.css';
+import {Link} from 'react-router-dom';
+
 
 class Home extends React.Component {
     render() {
         return(
-            <div className='Homepage'>
-                <h2>IronProfile</h2>
-                <p>Today we will create an app with authoritation, adding some cool styles!</p>
-                <button>Signup</button>
-                <button>Login</button>
+            <div className='homepage'>
+                <div className='text-container'>
+                    <h2>IronProfile</h2>
+                    <p>Today we will create an app with authoritation, adding some cool styles!</p>
+                    <Link to='/signup'>Signup</Link>
+                    <Link to='/login'>Login</Link>
+                </div>
             </div>
         )
     }
